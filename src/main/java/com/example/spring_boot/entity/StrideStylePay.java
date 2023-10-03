bdb13ed0dc0be33863d424ce34b7c031b6e5da72
Pham Tu <tupaph13622@fpt.edu.vn>
2023-10-03 12:43:32 +0700

    tu push code

---
+++ src/main/java/com/example/spring_boot/entity/StrideStylePay.java
@@ -0,0 +1,41 @@
+package com.example.spring_boot.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@SuppressWarnings("serial")
+@Data
+@Entity
+@Table(name = "stridestylepay")
+public class StrideStylePay implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double surplus;
+
+    @Column(name = "isDelete")
+    private Boolean isDelete = false;
+
+    @JsonIgnore
+    @OneToOne
+    @JoinColumn(name = "idcustomer", updatable = false, insertable = false)
+    private Customer customer;
+
+    @Column(name = "idcustomer")
+    private Long idcustomer;
+
+//    @JsonIgnore
+//    @OneToOne
+//    @JoinColumn(name = "idcustomer", updatable = false, insertable = false)
+//    private HistoryPay historyPay;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "stridestylepay")
+    private List<HistoryPay> historyPays;
+
+}
